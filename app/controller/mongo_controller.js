6a30389fd02a5abc4863b5c152f2d55bc6de88d6
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-06-23 15:11:21 -0700

    updating commit fixed notes

--- app/controller/mongo_controller.js
+++ app/controller/mongo_controller.js
@@ -8,9 +8,9 @@ var db = require("../models");
 var axios = require("axios");
 var cheerio = require("cheerio");
 module.exports = function(app) {
-  app.get("/scrape", function(req, res) {
+  app.get("/", function(req, res) {
     // First, we grab the body of the html with axios
-
+    console.log("get articles", "I'm here");
     axios
       .get("https://www.washingtonpost.com/")
       .then(function(response) {
@@ -55,8 +55,7 @@ module.exports = function(app) {
           .then(function(dbArticle) {
             // View the added result in
 
-            console.log(dbArticle);
-            res.render("index",{"article": dbArticle});
+            res.render("index", { article: dbArticle });
             //************************************** */
           })
           .catch(function(err) {
@@ -87,20 +86,23 @@ module.exports = function(app) {
       });
   });
 
-  app.get("/", function(req, res) {
+  /*  app.get("/", function(req, res) {
     // Grab every document in the Articles collection
 
     res.render("index");
   });
-
+ */
   // Route for grabbing a specific Article by id, populate it with it's note
   app.get("/articles/:id", function(req, res) {
     // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    event.stopPropagation();
+    console.log("inside post");
     db.Article.findOne({ _id: req.params.id })
       // ..and populate all of the notes associated with it
       .populate("note")
       .then(function(dbArticle) {
         // If we were able to successfully find an Article with the given id, send it back to the client
+        console.log("inside post then");
         res.json(dbArticle);
       })
       .catch(function(err) {
@@ -110,13 +112,16 @@ module.exports = function(app) {
   });
 
   // Route for saving/updating an Article's associated Note
-  app.post("/articles/:id", function(req, res) {
+  app.post("/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
+    console.log("inside post article note");
     db.Note.create(req.body)
       .then(function(dbNote) {
         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
         // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        console.log("inside post article then");
+
         return db.Article.findOneAndUpdate(
           { _id: req.params.id },
           { note: dbNote._id },
