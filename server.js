991a8fff07cacd3ed90d7c2dc9df355f6d264c5d
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-06-24 15:22:28 -0700

    adding readme

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + "/app"));
 // Require all models
 var db = require("./app/models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
